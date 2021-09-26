37fbb05db18c620e7e04a242607d79feb94df88c
jimmymendy <jimmymendy@Pros-MacBook-Pro.local>
2021-09-26 15:13:20 +0100

    add products page

---
+++ pages/products.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const products = () => {
+  return (
+    <>
+     <h1>Cool prodcut 1</h1>
+     <p>This is a very cool prodcut</p>
+     <p>£2.50</p>
+     <hr /> 
+     <h1>Cool prodcut 2</h1>
+     <p>This is a very cool prodcut</p>
+     <p>£500.50</p>
+    </>
+  )
+}
+
+export default products
